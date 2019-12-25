30fc0f4ff01b625fc55ad9e3aedba1a3742cdcfd
shishan <cclin578089476@gmail.com>
2019-12-25 23:13:56 +0800

    添加代码12.25

--- src/main/java/com/dragon/netty/helper/watchers/WatchThreadPool.java
+++ src/main/java/com/dragon/netty/helper/watchers/WatchThreadPool.java
@@ -1,4 +1,67 @@
 package com.dragon.netty.helper.watchers;
 
-public class WatchThreadPool {
+import com.dragon.netty.helper.interfaces.ICloseable;
+import com.dragon.netty.helper.interfaces.IInitable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static com.dragon.netty.utils.ThreadsUtils.createSyncPool;
+import static com.dragon.netty.utils.ThreadsUtils.syncClosePool;
+
+/**
+ * 公共监听线程池
+ */
+public class WatchThreadPool implements IInitable, ICloseable {
+    private final static Logger logger = LoggerFactory.getLogger(WatchThreadPool.class);
+
+    //线程池对象
+    private ThreadPoolExecutor pool = null;
+
+    //线程池配置
+    private final int CORE_POOL_SIZE = 5;
+    private final int MAX_POOL_SIZE = 10;
+    private final int KEEPALIVE_TIME = 1;
+
+    /**
+     * 线程池的关闭
+     */
+    @Override
+    public void close() {
+        syncClosePool(pool);
+        logger.info("[{}]关闭", this.getClass().getSimpleName());
+    }
+
+    /**
+     * 线程池的初始化
+     */
+    @Override
+    public void init() {
+        this.pool = createSyncPool(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEPALIVE_TIME);
+        logger.info("监听线程池启动，最大容量[{}]", MAX_POOL_SIZE);
+    }
+
+    /**
+     * 添加任务
+     * 线程池满了不能添加
+     *
+     * @param runner 需要添加的线程
+     */
+    public synchronized void addRunner(Runnable runner) {
+        if (pool != null) {
+            //检查任务是否可行
+            String runName = runner.getClass().getSimpleName();
+            if (pool.getActiveCount() < MAX_POOL_SIZE) {
+                pool.execute(runner);
+                logger.info("监听线程池添加任务[{}]成功", runName);
+
+            } else {
+                logger.warn("监听线程池添加任务[{}]失败：任务数超过最大线程数[{}]", runName, MAX_POOL_SIZE);
+            }
+        } else {
+            logger.error("监听线程池添加任务失败：pool为null");
+        }
+    }
+
 }
--- src/main/java/com/dragon/netty/utils/ThreadsUtils.java
+++ src/main/java/com/dragon/netty/utils/ThreadsUtils.java
@@ -3,10 +3,19 @@ package com.dragon.netty.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadsUtils {
     private final static Logger logger = LoggerFactory.getLogger(ThreadsUtils.class);
 
-
+    /**
+     * 线程睡眠
+     *
+     * @param mills 睡眠时间
+     */
     public static void threadSleep(long mills) {
         try {
             if (mills > 0) {
@@ -14,6 +23,49 @@ public class ThreadsUtils {
             }
         } catch (InterruptedException e) {
             logger.warn("线程[{}]休眠异常", Thread.currentThread().getName(), e);
+        }
+    }
+
+    /**
+     * 异步的方式创建线程数池
+     *
+     * @param corePoolSize  池的数
+     * @param maxPoolSize   池的最多容纳数
+     * @param keepAliveTime 存活的时间
+     * @return
+     */
+    public static ThreadPoolExecutor createSyncPool(int corePoolSize, int maxPoolSize, int keepAliveTime) {
+        ThreadPoolExecutor.CallerRunsPolicy policy = new ThreadPoolExecutor.CallerRunsPolicy();
+        return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.SECONDS, new SynchronousQueue<>(), policy);
+    }
+
+    public static ThreadPoolExecutor createPool(int corePoolSize, int maxPoolSize, int keepAliveTime, int queueCapacity) {
+        LinkedBlockingDeque queue = new LinkedBlockingDeque(queueCapacity);
+        ThreadPoolExecutor.CallerRunsPolicy policy = new ThreadPoolExecutor.CallerRunsPolicy();
+        return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.SECONDS, queue, policy);
+    }
+
+    /**
+     * 关闭线程池
+     *
+     * @param executor 执行的线程
+     */
+    public static void syncClosePool(ThreadPoolExecutor executor) {
+        if (executor != null) {
+            //不再添加新任务到线程池
+            executor.shutdown();
+            //延迟关闭超时则强制关闭
+            if (!executor.isTerminated()) {
+                try {
+                    executor.shutdown();
+                } catch (Exception e) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("线程池强制关闭异常，异常信息: " + e.getMessage(), e);
+                    }
+                }
+            } else {
+                logger.warn("线程池为null,不执行关闭!");
+            }
         }
     }
 }
